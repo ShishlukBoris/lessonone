2e594c0e9606d9dccb57685fb772aa7cbbfe8a8f
Shishluk <ShislukBoris@gmail.com>
2021-05-27 18:54:17 +0300

    Initial Commit

---
+++ lessonone/lessononeApp.swift
@@ -0,0 +1,17 @@
+//
+//  lessononeApp.swift
+//  lessonone
+//
+//  Created by Boris on 27.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct lessononeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
